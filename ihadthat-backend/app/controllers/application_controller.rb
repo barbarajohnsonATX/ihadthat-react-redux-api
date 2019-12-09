163154d40beb603036a413c808219c72e3fd0cc5
Barbara Johnson <johnson.barbara@gmail.com>
2019-12-08 22:56:41 -0600

    Create react app

--- ihadthat-backend/app/controllers/application_controller.rb
+++ ihadthat-backend/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    # helper_method :current_user, :logged_in?
+
+
+    # def current_user
+    #     User.first
+    # end 
+
+    # def logged_in?
+    #     !!current_user
+    # end 
+
 end
--- ihadthat-backend/config/routes.rb
+++ ihadthat-backend/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :toy_ownerships
   namespace :api do 
     namespace :v1 do 
       resources :users
       resources :toys
+      resources :toy_ownerships
+
     end 
   end 
  end
