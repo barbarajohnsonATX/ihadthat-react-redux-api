788bed3180dd773d842e0677dac54f26ff06e4e2
Barbara Johnson <johnson.barbara@gmail.com>
2019-12-09 22:58:58 -0600

    user login in redux

--- ihadthat-backend/app/controllers/application_controller.rb
+++ ihadthat-backend/app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::API
     # helper_method :current_user, :logged_in?
 
 
-    # def current_user
-    #     User.first
-    # end 
+    def current_user
+        User.find(session[:user_id])
+    end 
 
-    # def logged_in?
-    #     !!current_user
-    # end 
+    def logged_in?
+        !!current_user
+    end 
 
 end
--- ihadthat-frontend/src/App.js
+++ ihadthat-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
- import './App.css';
+import './App.css';
+import Login from "./components/Login"
 
 class App extends React.Component {
 
@@ -9,6 +10,7 @@ class App extends React.Component {
     return (
       <div className="App">
          Hello
+         <Login />
       </div>
     );
   }
